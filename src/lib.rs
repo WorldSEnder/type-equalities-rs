001fd1d60dafdda133f7f31b28a122c7f79d1b0f
Martin Molzer <ga65guy@mytum.de>
2021-05-06 07:29:40 +0200

    fix year in copyright

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-// Copyright (c) 2015 t WorldSEnder
+// Copyright (c) 2021 t WorldSEnder
 // Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
 // license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
 // at your option. All files in the project carrying such
 // notice may not be copied, modified, or distributed except
